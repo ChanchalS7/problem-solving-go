a74e25189a436c02659bbb7ccd0212dacb3937a6
ChanchalS7 <chanchal.verma@stackguardian.io>
2024-08-05 18:48:32 +0530

    add reverse vowels of a striing

--- Can Place Flowers/main.go
+++ Can Place Flowers/main.go
@@ -1,27 +1,34 @@
 package main
 
-
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    l := len(flowerbed)
-    if n == 0 { return true }
-    if l == 1 && flowerbed[0] == 0 && n == 1 { return true }
-    if l == 2 && flowerbed[0] == 0 && flowerbed[1] == 0 && n == 1 { return true }
-    for i := 1; i < l - 1; i++ {
-        if i-1 == 0 && flowerbed[i-1] == 0 && flowerbed[i] == 0 {
-            flowerbed[i-1] = 1
-            n--
-        }
-        if i+1 == l-1 && flowerbed[i+1] == 0 && flowerbed[i] == 0 {
-            flowerbed[i+1] = 1
-            n--
-        }
-        if flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
-            flowerbed[i] = 1
-            n--
-        }
-        if n <= 0 { return true }
-    }
-    return false
+	l := len(flowerbed)
+	if n == 0 {
+		return true
+	}
+	if l == 1 && flowerbed[0] == 0 && n == 1 {
+		return true
+	}
+	if l == 2 && flowerbed[0] == 0 && flowerbed[1] == 0 && n == 1 {
+		return true
+	}
+	for i := 1; i < l-1; i++ {
+		if i-1 == 0 && flowerbed[i-1] == 0 && flowerbed[i] == 0 {
+			flowerbed[i-1] = 1
+			n--
+		}
+		if i+1 == l-1 && flowerbed[i+1] == 0 && flowerbed[i] == 0 {
+			flowerbed[i+1] = 1
+			n--
+		}
+		if flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
+			flowerbed[i] = 1
+			n--
+		}
+		if n <= 0 {
+			return true
+		}
+	}
+	return false
 }
 
 /*
